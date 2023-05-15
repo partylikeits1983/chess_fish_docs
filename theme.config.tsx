d14d2c418dda9a165c1bf888fcf5242ef2f59bd2
socrates <aleqvids@gmail.com>
2023-05-15 16:10:03 +0300

    updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,38 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Chess.fish Docs</span>,
-  project: {
-    link: 'https://github.com/',
+logo:  <strong>Chess.Fish Docs</strong>
+
+,
+project: {
+link: 'https://github.com/partylikeits1983',
+},
+chat: {
+link: 'https://discord.com',
+},
+docsRepositoryBase: 'https://github.com/partylikeits1983',
+footer: {
+text: 'Chess.fish Docs',
+},
+
+ useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Chess.fish'
+    }
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Chess.fish" />
+      <meta property="og:description" content="Chess on the Blockchain" />
+      
+    </>
+  ),
+
+  primaryHue: { dark: 155, light: 145},
+
+  faviconGlyph: '♕'
 }
 
 export default config
