d0e08b8f4e36814a9fa9b8150a07473bd0b54f61
riemann <aleqvids@gmail.com>
2023-08-25 14:40:44 +0300

    updates

---
+++ pages/concepts/board.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const ChessBoard: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [board, setBoard] = useState('');
+
+  const generateBoard = (pieces: string[]) => {
+    let boardString = "   +------------------------+\n ";
+    for (let i = 0; i < 64; i++) {
+      if (i % 8 === 0) {
+        let row_num = 8 - i / 8;
+        boardString += String(row_num) + "  ";
+      }
+
+      boardString += " " + pieces[i] + " ";
+
+      if ((i + 1) % 8 === 0) {
+        boardString += "\n ";
+      }
+    }
+
+    boardString += "  +------------------------+\n";
+    boardString += "     a  b  c  d  e  f  g  h";
+    setBoard(boardString);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    generateBoard(e.target.value.split(''));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter pieces string"
+      />
+      <pre>{board}</pre>
+    </div>
+  );
+};
+
+export default ChessBoard;
